c34fe3ef8dc660b6cbee0fde0a4f9ad92073eecb
F-loat <chaimaoyuan@foxmail.com>
2019-12-28 11:33:32 +0800

    feat: 使用示例

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <NcformMaker v-model="schema">
+      <template v-slot:headerTools>
+        <NcmakerFormClear
+          :schema="schema"
+          @clear="clearComp"
+        />
+        <NcmakerFormPreview
+          :formSchema="schema"
+        />
+        <NcmakerFormEditor
+          :schema="schema"
+          @edit="changeSchema"
+        />
+        <NcmakerFormTemplate
+          @changeSchema="changeSchema"
+        />
+        <NcmakerFormSave
+          :schema="schema"
+          type="template"
+          icon="el-icon-document-add"
+        />
+        <NcmakerFormSave
+          :schema="schema"
+          type="form"
+          icon="el-icon-s-promotion"
+        />
+      </template>
+    </NcformMaker>
+  </div>
+</template>
+
+<script>
+import { NcformMaker, NcformMakerToolComps } from 'ncform-maker'
+
+export default {
+  components: {
+    NcformMaker,
+    ...NcformMakerToolComps
+  },
+  data () {
+    return {
+      schema: {
+        type: 'object',
+        properties: {}
+      }
+    }
+  },
+  methods: {
+    clearComp () {
+      this.schema = {
+        type: 'object',
+        properties: {}
+      }
+    },
+    changeSchema (schema) {
+      this.schema = schema
+    }
+  }
+}
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  height: 100vh;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import vueNcform from '@f-loat/ncform'
+import VueI18n from 'vue-i18n'
+import { NcmakerMessages } from 'ncform-maker'
+
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import ncformStdComps from '@f-loat/ncform-theme-elementui'
+import { controlComps, layoutComps, businessComps } from '@f-loat/ncform-comps'
+
+import App from './App.vue'
+
+Vue.use(Element)
+Vue.use(VueI18n)
+Vue.use(vueNcform, {
+  extComponents: Object.assign(ncformStdComps, layoutComps, controlComps, businessComps),
+  lang: 'zh-cn'
+})
+Vue.config.productionTip = false
+
+const i18n = new VueI18n({ locale: 'zh-CN', messages: NcmakerMessages })
+
+new Vue({
+  i18n,
+  render: h => h(App)
+}).$mount('#app')
